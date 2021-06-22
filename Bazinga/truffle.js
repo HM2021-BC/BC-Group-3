a07e5546d99445b250751a5be9fcadf549a4d435
Fabian Rittmeier <f.rittmeier@hm.edu>
2021-06-22 21:53:11 +0000

    Truffle compile and migrate.

---
+++ Bazinga/truffle.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // see <http://truffleframework.com/docs/advanced/confguartion>
+  // to customize your truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "*"
+    }
+  }
+};

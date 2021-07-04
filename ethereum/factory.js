a57cffbf23cf81a8b8196c10518129f161b24b88
Simon Hirner <>
2021-07-04 20:42:02 +0000

    Change address.

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,8 +1,8 @@
 import web3 from './web3';
 import MarketplaceFactory from './build/contracts/MarketplaceCreator.json';
 
-const marketplaceFactoryAddress = "0xb0A6A09Fba1133fe228351178B2409221dD331cB";
+const marketplaceFactoryAddress = "0x52754F331Dd52F16D0f38693B0126420AC2eB3Ea";
 
 const instance = new web3.eth.Contract(MarketplaceFactory.abi, marketplaceFactoryAddress);
 
-export default instance;  
+export default instance;

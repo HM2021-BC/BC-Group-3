0d049bbdfb5760da336368f0817ec1373f599b2c
Simon Hirner <>
2021-07-05 10:10:00 +0000

    New deployment.

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 import MarketplaceFactory from './build/contracts/MarketplaceCreator.json';
 
-const marketplaceFactoryAddress = "0x52754F331Dd52F16D0f38693B0126420AC2eB3Ea";
+const marketplaceFactoryAddress = "0xDeBF84434A03A9FeD93B38bf10a33d95cCc9F1D1";
 
 const instance = new web3.eth.Contract(MarketplaceFactory.abi, marketplaceFactoryAddress);
 
